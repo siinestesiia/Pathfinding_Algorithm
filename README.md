61c3c0ad50e0cd553058c4ba8728474cab02a67c
siinestesiia <emir-cykman@hotmail.com>
2025-08-24 23:19:45 -0300

    A* Algorithm now working. Minor adjustments needed.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # A* Algorithm (Traditional AI for games)
 
 This is meant to be a practice and study on the technique for pathfinding. It's implemented using Pygame since it's a two dimensional grid containing nodes.
-The idea is to have a grid containing nodes (light grey squares), and be able to select a **Start Node** (currently green), and **End Node** (currently red); as well as obstacles (dark grey nodes) that will be excluded in the process of calculating the path from **Start node** to **End node**. The path would be a list of nodes (displayed as light blue).
+The idea is to have a grid containing nodes, and be able to select a **Start Node**, and **End Node**; as well as obstacles that will be excluded in the process of calculating the path from **Start node** to **End node**.
 
 For calculating the distance between the two points in this grid-based system, I'll use the *Manhattan Distance* formula:
 **|X<sub>1</sub> - X<sub>2</sub>| + |Y<sub>1</sub> - Y<sub>2</sub>|**
 
 ## Screenshot Preview
 
-Currently the program lets you select start node, end node and non-walkable nodes.
-The core A* algorithm is yet to be developed.
-![Working_Grid](Preview/Pathfinder_001.png)
+Currently the program lets you select start node (*green node*), end node (*red node*) and non-walkable nodes (*dark-grey nodes*).
+The discarted nodes are displayed in **Blue**, the considered nodes are displayed in **Orange**, and the actual path is displayed in **Yellow**.
+![Working_Grid](Preview/Pathfinder_002.png)
--- pathfinder.py
+++ pathfinder.py
@@ -23,6 +23,11 @@ class PathfinderApp():
         self.walkable_color = (150, 150, 150, 255) # Light grey.
         self.unwalkable_color = (50, 50, 50, 255) # Dark grey.
 
+        # A* Pathfinding colors
+        self.open_color = (0, 150, 255) # Blue
+        self.closed_color = (255, 165, 0) # Orange
+        self.path_color = (255, 255, 0) # Yellow
+
         self.run_app()
 
 
@@ -38,6 +43,12 @@ class PathfinderApp():
                         self.select_node(mouse_pos, event.button)
                     elif event.button == 3:
                         self.select_node(mouse_pos, event.button)
+                # Start finding a path event
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if self.start_node != None and self.end_node != None:
+                            self.reset_grid()
+                            self.find_path()
                 if event.type == pygame.QUIT:
                     pygame.quit() # Terminate the game.
                     exit() # from sys module, to properly quit.
@@ -130,23 +141,109 @@ class PathfinderApp():
         grid_gap_x = node_width + offset
         grid_gap_y = node_height + offset
 
-        # Account for the initial offset on left and top
+        # Account for the initial offset on left and top.
         adjusted_x = mouse_x - offset
         adjusted_y = mouse_y - offset
 
-        # calculate the grid column and row
+        # calculate the grid column and row.
         col = adjusted_x // grid_gap_x
         row = adjusted_y // grid_gap_y
 
-        # Check if the calculated indices are within the grid bounds
+        # Check if the calculated indices are within the grid bounds.
         if 0 <= row < len(self.grid) and 0 <= col < len(self.grid[0]):
-            # Now check if the click was actually inside the node's rect
+            # Now check if the click was actually inside the node's rect.
             node = self.grid[row][col]
             if node.node_rect.collidepoint(mouse_pos):
                 return node
         
         return None
 
+    def find_path(self):
+        # A list of nodes to be evaluated, sorted by f_cost.
+        open_set = []
+        # A set of nodes already evaluated.
+        closed_set = set()
+
+        # Start the algorithm
+        self.start_node.g_cost = 0
+        self.start_node.calculate_h_cost(self.end_node)
+        open_set.append(self.start_node) # Add the start node
+
+        # Main loop for A*
+        while open_set:
+            # Sort the set and get the node with lowest f_cost.
+            open_set.sort()
+            current_node = open_set.pop(0)
+
+            # Move the current node the the closed set.
+            closed_set.add(current_node)
+            current_node.color = self.closed_color
+
+            # If the current node is the end node, you found the path.
+            if current_node == self.end_node:
+                self.reconstruct_path(current_node)
+                self.start_node.color = self.start_color
+                self.end_node.color = self.end_color
+                print('Path found!')
+                return
+            
+            # Iterate trhough the neighbors of the current node.
+            for neighbor in self.get_neighbors(current_node):
+                # If the neighbor is not walkable or has been evaluated, skip it.
+                if not neighbor.walkable or neighbor in closed_set:
+                    continue
+
+                # The g_cost to the neighbor is the g_cost of the current node + 1
+                new_g_cost = current_node.g_cost + 1
+
+                # If a better path to the neighbor is found:
+                if new_g_cost < neighbor.g_cost:
+                    # Update neighbor's costs and parent.
+                    neighbor.parent = current_node
+                    neighbor.g_cost = new_g_cost
+                    neighbor.calculate_h_cost(self.end_node)
+
+                    # Add the neighbor to the open set if isn't already there.
+                    if neighbor not in open_set:
+                        open_set.append(neighbor)
+                        neighbor.color = self.open_color
+
+        print('No path found.')   
+
+    def get_neighbors(self, node):
+        neighbors = []
+        row, col = node.grid_coordinates
+
+        # Directions: up, down, left, right.
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+        for d_row, d_col in directions:
+            neighbor_row, neighbor_col = row + d_row, col + d_col
+
+            # Check if the neighbor is within the grid bounds.
+            if 0 <= neighbor_row < len(self.grid) and 0 <= neighbor_col < len(self.grid[0]):
+                neighbor = self.grid[neighbor_row][neighbor_col]
+                neighbors.append(neighbor)
+
+        return neighbors
+
+    def reconstruct_path(self, current_node):
+        temp_node = current_node
+        while temp_node.parent:
+            temp_node.color = self.path_color
+            temp_node = temp_node.parent
+
+    # After finding a path, clears all nodes
+    def reset_grid(self):
+        for row in self.grid:
+            for node in row:
+                if node.walkable:
+                    node.color = self.walkable_color
+                    # Reset all the A* costs and parent
+                    node.g_cost = float('inf')
+                    node.h_cost = float('inf')
+                    node.f_cost = float('inf')
+                    node.parent = None
 
 
 if __name__ == '__main__':
